299127bc1f15615a1ebee4397954d050e3f24df1
Progga <proggaroy2000@gmail.com>
2023-02-13 23:22:23 +0600

    add onclick on button using function

---
+++ js/event.js
@@ -0,0 +1,30 @@
+function lightPink(){
+    document.body.style.backgroundColor='lightPink'
+}
+function lightBlue(){
+    document.body.style.backgroundColor='lightBlue'
+}
+function lightGreen(){
+    document.body.style.backgroundColor='lightGreen'
+}
+function lavender (){
+    document.body.style.backgroundColor='lavender'
+}
+
+const yellowButton = document.getElementById('yellow-bg');
+yellowButton.onclick = yellowBtn;
+
+function yellowBtn(){
+    document.body.style.backgroundColor='lightYellow'
+}
+
+const purpleButton = document.getElementById('purple-bg');
+purpleButton.onclick = function purpleBtn(){
+    document.body.style.backgroundColor = 'salmon'
+}
+// const purpleButton = document.getElementById('purple-bg');
+// purpleButton.onclick= purpleBtn;
+
+// function purpleBtn(){
+//     document.body.style.backgroundColor = 'gray'
+// }
